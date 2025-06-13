6b2fdadee993968a3003aeecd720343824dae666
miraslauli <mirchyhir@gmail.com>
2025-06-13 19:58:10 +0200

    upd

--- generate_ssl.sh
+++ generate_ssl.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Tworzenie certyfikatu SSL dla środowiska deweloperskiego
-echo "Tworzenie certyfikatu SSL z podpisem własnym..."
-
 # Tworzymy katalog dla certyfikatów
 mkdir -p ssl
 
@@ -10,15 +7,8 @@ mkdir -p ssl
 openssl genrsa -out ssl/key.pem 2048
 
 # Generujemy certyfikat
-openssl req -new -x509 -key ssl/key.pem -out ssl/cert.pem -days 365 -subj "/C=BY/ST=Minsk/L=Minsk/O=University/OU=IT/CN=localhost"
+openssl req -new -x509 -key ssl/key.pem -out ssl/cert.pem -days 365 -subj "/C=PL/ST=Warsaw/L=Warsaw/O=University/OU=IT/CN=localhost"
 
 # Ustawiamy uprawnienia dostępu
 chmod 600 ssl/key.pem
 chmod 644 ssl/cert.pem
-
-echo "Certyfikat SSL został utworzony w katalogu ssl/"
-echo "Certyfikat: ssl/cert.pem"
-echo "Klucz prywatny: ssl/key.pem"
-echo ""
-echo "UWAGA: To jest certyfikat z podpisem własnym do celów deweloperskich!"
-echo "W środowisku produkcyjnym użyj certyfikatu od zaufanego CA (np. Let's Encrypt)" 
